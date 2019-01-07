0eee857641afcf1bc89fe24e824ffdd91eb9501c
Mathieu <mathieu.st-yves@hotmail.com>
2019-01-07 16:24:53 -0500

    remove password

--- config/app.php
+++ config/app.php
@@ -256,7 +256,7 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'root',
-            'password' => '4N-*TCTOk*9h',
+            'password' => '',
             'database' => 'my_app',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
