f2d97ef04b3770ea59d2406539d8e8852ede1436
mstyves <mathieu.st-yves@hotmail.com>
2019-01-20 13:10:31 -0500

    test database

--- config/schema/sessions.sql
+++ config/schema/sessions.sql
@@ -4,6 +4,7 @@
 # For full copyright and license information, please see the LICENSE.txt
 # Redistributions of files must retain the above copyright notice.
 # MIT License (https://opensource.org/licenses/mit-license.php)
+USE my_app;
 
 CREATE TABLE `sessions` (
   `id` char(40) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
@@ -13,3 +14,48 @@ CREATE TABLE `sessions` (
   `expires` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created DATETIME,
+    modified DATETIME
+);
+
+CREATE TABLE articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    slug VARCHAR(191) NOT NULL,
+    body TEXT,
+    published BOOLEAN DEFAULT FALSE,
+    created DATETIME,
+    modified DATETIME,
+    UNIQUE KEY (slug),
+    FOREIGN KEY user_key (user_id) REFERENCES users(id)
+) CHARSET=utf8mb4;
+
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(191),
+    created DATETIME,
+    modified DATETIME,
+    UNIQUE KEY (title)
+) CHARSET=utf8mb4;
+
+CREATE TABLE articles_tags (
+    article_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (article_id, tag_id),
+    FOREIGN KEY tag_key(tag_id) REFERENCES tags(id),
+    FOREIGN KEY article_key(article_id) REFERENCES articles(id)
+);
+
+INSERT INTO users (email, password, created, modified)
+VALUES
+('cakephp@example.com', 'sekret', NOW(), NOW());
+
+INSERT INTO articles (user_id, title, slug, body, published, created, modified)
+VALUES
+(1, 'First Post', 'first-post', 'This is the first post.', 1, now(), now());
